6925728f5b00632dbb965563c46884ed989e6baa
Prajwal Goswami <82706668+P7rox-dev@users.noreply.github.com>
2021-09-24 23:58:12 +0530

    Update IndexServiceImpl.java

--- src/main/java/com/p7rox/esApi/service/v1/IndexServiceImpl.java
+++ src/main/java/com/p7rox/esApi/service/v1/IndexServiceImpl.java
@@ -1,29 +1,79 @@
 package com.p7rox.esApi.service.v1;
 
-import com.p7rox.esApi.entity.Product;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.elasticsearch.action.search.*;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class IndexServiceImpl implements IndexService{
+public class IndexServiceImpl implements IndexService {
 
     private final RestHighLevelClient restHighLevelClient;
+    private final ObjectMapper objectMapper;
 
-    public Optional<String> getDocument(String index, String id) {
+    public String getDocument(String index, String id) {
         return "true";
     }
 
-    public List<String> getDocuments(String index, Map<String,String> allParams) {
+    public List<Object> getDocuments(String index, Map<String, String> allParams) {
 
-        
-        List<String> documentList = new ArrayList<>();
-        return documentList;
+        List<Object> resultList = new ArrayList<>();
+
+        SearchRequest searchRequest = new SearchRequest(index);
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.matchAllQuery());
+        searchSourceBuilder.size(100);
+        searchRequest.source(searchSourceBuilder);
+        searchRequest.scroll(TimeValue.timeValueMinutes(1L));
+
+        try {
+            SearchResponse searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+            String scrollId = searchResponse.getScrollId();
+            SearchHits hits = searchResponse.getHits();
+            resultList.addAll(toStringList(hits.getHits()));
+            do {
+//                System.out.println(hits.getTotalHits().value);
+//                System.out.println(hits.getHits().length);
+//                System.out.println(scrollId);
+
+                SearchScrollRequest scrollRequest = new SearchScrollRequest(scrollId);
+                scrollRequest.scroll(TimeValue.timeValueSeconds(30));
+                SearchResponse searchScrollResponse = restHighLevelClient.scroll(scrollRequest, RequestOptions.DEFAULT);
+                scrollId = searchScrollResponse.getScrollId();
+                hits = searchScrollResponse.getHits();
+                resultList.addAll(toStringList(hits.getHits()));
+
+            } while (scrollId != null && hits.getHits().length != 0 );
+
+            ClearScrollRequest request = new ClearScrollRequest();
+            request.addScrollId(scrollId);
+            ClearScrollResponse response = restHighLevelClient.clearScroll(request, RequestOptions.DEFAULT);
+
+            return resultList;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private List<Object> toStringList(SearchHit[] searchHits) throws Exception {
+        List<Object> stringList = new ArrayList<>();
+        for (SearchHit searchHit : searchHits) {
+            stringList.add(searchHit.getSourceAsMap());
+        }
+        return stringList;
     }
 }
